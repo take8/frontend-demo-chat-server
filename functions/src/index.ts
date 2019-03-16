6520a9dd738c9f0a95fddc648a0d03bc38bdb607
take8 <37664118+take8@users.noreply.github.com>
2019-03-16 16:02:00 +0900

    KoaからExpressに切り替え

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,8 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
-import * as Koa from "koa";
-import * as Router from "koa-router";
-import * as bodyParser from "koa-bodyparser";
-import * as cors from "@koa/cors";
+import * as Express from "express";
+import * as cors from "cors";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -15,8 +13,8 @@ import * as cors from "@koa/cors";
 
 admin.initializeApp(functions.config().firebase);
 
-const app = new Koa();
-const router = new Router();
+const app = Express();
+const router = Express.Router();
 
 interface UserInterface {
   id: string,
@@ -36,10 +34,10 @@ const anonymousUser: UserInterface = {
   avatar: ""
 };
 
-const checkUser = async (ctx: Koa.Context, next: Function) => {
-  ctx.user = anonymousUser;
-  if (ctx.query.auth_token !== undefined) {
-    const idToken = ctx.query.auth_token;
+const checkUser = async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
+  req.user = anonymousUser;
+  if (req.query.auth_token !== undefined) {
+    const idToken = req.query.auth_token;
 
     const decodedIdTaken = await admin.auth().verifyIdToken(idToken)
     .catch(async error => {
@@ -51,7 +49,7 @@ const checkUser = async (ctx: Koa.Context, next: Function) => {
       name: decodedIdTaken.name,
       avatar: decodedIdTaken.picture
     };
-    ctx.user = authUser;
+    req.user = authUser;
     await next();
   } else {
     await next();
@@ -92,15 +90,14 @@ async function createChannel(channelName: string) {
   await channelRef.child(channelName).set(JSON.parse(defaultData));
 }
 
-router.post("/channels", async (ctx: Koa.Context) => {
-  const channelName = ctx.body.channelName;
+router.post("/channels", async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
+  const channelName = req.body.channelName;
   await createChannel(channelName);
-  ctx.type = "application/json; charset=utf-8";
-  ctx.status = 201;
-  ctx.body = { result: "ok" };
+  res.header("Content-Type", "application/json; charset=utf-8");
+  res.status(201).send({ result: "ok" });
 });
 
-router.get("/channels", async (ctx: Koa.Context) => {
+router.get("/channels", async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
   const channelRef = admin.database().ref("channels");
   await channelRef.once("value", snapshot => {
     const items = new Array();
@@ -108,27 +105,26 @@ router.get("/channels", async (ctx: Koa.Context) => {
       const channelName = childSnapshot.key;
       items.push(channelName);
     });
-    ctx.type = "application/json; charset=utf-8";
-    ctx.body = { channels: items };
+    res.header("Content-Type", "application/json; charset=utf-8");
+    res.send({ channels: items });
   });
 });
 
-router.post("/channels/:channelName/messages", (ctx: Koa.Context) => {
-  const channelName = ctx.params.channelName;
+router.post("/channels/:channelName/messages", (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
+  const channelName = req.params.channelName;
   const message: MessageInterface = {
     date: new Date().toJSON(),
-    body: ctx.body.body,
-    user: ctx.user
+    body: req.body.body,
+    user: req.user
   };
   const messageRef = admin.database().ref(`channels/${channelName}/messages`);
   messageRef.push(message);
-  ctx.type = "application/json; charset=utf-8";
-  ctx.status = 201;
-  ctx.body = { result: "ok" };
+  res.header("Content-Type", "application/json; charset=utf-8");
+  res.status(201).send({ result: "ok" });
 });
 
-router.get("/channels/:channelName/messages", async (ctx: Koa.Context) => {
-  const channelName = ctx.params.channelName;
+router.get("/channels/:channelName/messages", async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
+  const channelName = req.params.channelName;
   const messageRef = admin.database().ref(`channels/${channelName}/messages`)
       .orderByChild("date")
       // 最後から20件
@@ -141,110 +137,21 @@ router.get("/channels/:channelName/messages", async (ctx: Koa.Context) => {
       items.push(message);
     });
     items.reverse();
-    ctx.type = "application/json; charset=utf-8";
-    ctx.body = { messages: items };
+    res.header("Content-Type", "application/json; charset=utf-8");
+    res.send({ messages: items });
   });
 });
 
-router.post("/reset", async (ctx: Koa.Context) => {
+router.post("/reset", async (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
   await createChannel("general");
   await createChannel("random");
-  ctx.type = "application/json; charset=utf-8";
-  ctx.status = 201;
-  ctx.body = { result: "ok" };
+  res.header("Content-Type", "application/json; charset=utf-8");
+  res.status(201).send({ result: "ok" });
 });
-
-// router
-//   .get("/", (ctx: Koa.Context) => {
-//     ctx.body = "Hello, Test";
-//   });
-
-// router
-//   .post("/", (ctx: Koa.Context) => {
-//     console.log(ctx.request.body);
-//     ctx.body = "Hello, Test POST";
-//   });
-
-
-// router.get('/', (ctx: Koa.Context) => {
-//   ctx.body = "hello world";
-//   console.log("success");
-// });
-
-// router.post('/', (ctx: Koa.Context) => {
-//   console.log(ctx.type);
-//   ctx.body = "post hello world";
-//   console.log("post success");
-//   // console.log(ctx.body);
-// });
-
-// router
-//   .get('/api', async ctx => {
-//     console.log('Getting users from /api');
-//     ctx.body = [{ a:1, b:1}, {a:2, b:2}];
-//   });
-
-// router
-//   .post('/api', async ctx => {
-//     console.log('creating user for /api');
-//     const user = ctx.request.body;
-//     console.log(user);
-//     // creation logic goes here
-//     // raw input can be accessed from ctx.request.rawBody
-//     ctx.status = 201;
-//   });
 
 
 app
-  .use(cors({ origin: "*" }))
-  .use(bodyParser())
-  .use(router.routes())
-  .use(router.allowedMethods());
+  .use(cors({ origin: true }))
+  .use("/", router);
 
-// // x-response-time
- 
-// app.use(async (ctx, next) => {
-//   const start = Date.now(); // --(1)
-//   await next();
-//   const ms = Date.now() - start;    // --(6)
-//   ctx.set('X-Response-Time', `${ms}ms`);    // --(7)
-// });
- 
-// // logger
- 
-// app.use(async (ctx, next) => {
-//   const start = Date.now(); // --(2)
-//   await next();
-//   const ms = Date.now() - start;    // --(4)
-//   console.log(`${ctx.method} ${ctx.url} - ${ms}`);  // --(5)
-// });
- 
-// // response
- 
-// app.use(async ctx => {
-//   ctx.body = 'Hello World'; // --(3)
-// });
-
-// app を外部から呼び出せるようにする
-const config = {
-  port: 5000
-};
-const server = app.listen(config.port, () => {
-  console.log(`HITMers-server is running on port ${config.port}`);
-});
-
-module.exports = server;
-
-
-exports.v1 = functions.https.onRequest(app.callback());
-// exports.v1 = functions.https.onRequest(server._events.request);
-
-// app.listen(3000);
-// exports.v1 = functions.https.onRequest((request, response) => {
-//   // if(request.method !== "POST"){
-//   //   response.send(405, 'HTTP Method ' +request.method+' not allowed');
-//   // }
-//   // response.send(request.body);
-//   console.log("-----" + request.body);
-//   response.send(request.body);
-// });
+exports.v1 = functions.https.onRequest(app);
